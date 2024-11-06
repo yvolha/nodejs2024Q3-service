19fb424c62af1bc9ddd1949ca9a7f8b80c32d3cc
Volha Yakauleva <yvvolha@gmail.com>
2024-11-06 23:17:13 +0200

    feat: update the database to be an injectable

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,9 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
 import { Database } from "./database.type";
 
-export const DATABASE: Database = {
-  users: [],
-  artists: [],
-  albums: [],
-  tracks: [],
-  favorites: [],
+@Injectable()
+export class DATABASE implements Database {
+  users: [];
+  artists: [];
+  albums: [];
+  tracks: [];
+  favorites: [];
 }
