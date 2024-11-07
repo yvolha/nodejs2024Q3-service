cb78e58ccf9cdd181f71e1a89fb4a7bd9744729f
Volha Yakauleva <yvvolha@gmail.com>
2024-11-07 21:54:22 +0200

    refactor: move User interface to the revevant folder

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,3 @@
-export interface User {
-  id: string; // uuid v4
-  login: string;
-  password: string;
-  version: number; // integer number, increments on update
-  createdAt: number; // timestamp of creation
-  updatedAt: number; // timestamp of last update
-}
-
 export interface Artist {
   id: string; // uuid v4
   name: string;
